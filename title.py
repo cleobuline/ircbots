63e9a8683f90f4a647178f6f7c9c6acc56d54276
Patricia charlotte BENEDETTO <110346262+cleobuline@users.noreply.github.com>
2024-06-29 01:38:02 +0200

    Update title.py  user irc.bot library
    more clear

--- title.py
+++ title.py
@@ -1,183 +1,210 @@
-import socket
+import irc.bot
+import irc.strings
 import subprocess
 import re
 
-# Paramètres pour le serveur IRC et le bot
-SERVER = "labynet.fr"
-PORT = 6667
-CHANNEL = "#labynet"
-NICK = "title"
+class MusicBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, server, port, channel, nickname):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
 
-# Connexion au serveur IRC
-irc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc_socket.connect((SERVER, PORT))
-irc_socket.send(f"USER {NICK} {NICK} {NICK} :{NICK}\r\n".encode())
-irc_socket.send(f"NICK {NICK}\r\n".encode())
-irc_socket.send(f"JOIN {CHANNEL}\r\n".encode())
+    def on_welcome(self, connection, event):
+        connection.join(self.channel)
 
-# Fonction pour exécuter un script AppleScript
-def execute_applescript(script):
-    process = subprocess.Popen(['osascript', '-e', script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    if error:
-        return None, error.decode()
-    return output.decode().strip(), None
+    def on_join(self, connection, event):
+        connection.privmsg(self.channel, "Bonjour ! Tapez !help pour obtenir la liste des commandes disponibles.")
 
-# Fonction pour rechercher une playlist par nom partiel
-def find_partial_playlist(playlist_name, all_playlists):
-    matches = [p for p in all_playlists if re.search(playlist_name, p, re.IGNORECASE)]
-    return matches
+    def on_privmsg(self, connection, event):
+        self.do_command(event, event.arguments[0])
 
-# Boucle principale
-while True:
-    data = irc_socket.recv(4096).decode()
-    if data.find('PING') != -1:
-        irc_socket.send('PONG :pingis\n'.encode())
-    elif data.find('PRIVMSG') != -1:
-        user = data.split('!', 1)[0][1:]
-        message = data.split('PRIVMSG', 1)[1].split(':', 1)[1].strip()
+    def on_pubmsg(self, connection, event):
+        self.do_command(event, event.arguments[0])
 
-        if message.startswith('!title'):
-            title, error = execute_applescript('tell application "Music" to get {name } of current track')
-            artist, error = execute_applescript('tell application "Music" to get { artist} of current track')
-            album, error = execute_applescript('tell application "Music" to get { album } of current track')
-            if title:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :TITRE: {title} ARTIST: {artist} ALBUM: {album}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
+    def do_command(self, event, command):
+        nick = event.source.nick
+        connection = self.connection
 
-        elif message.startswith('!next'):
-            _, error = execute_applescript('tell application "Music" to next track')
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Piste suivante en cours de lecture\r\n".encode())
+        if command.startswith('!title'):
+            self.handle_title_command(connection)
+        elif command.startswith('!next'):
+            self.handle_next_command(connection)
+        elif command.startswith('!prev'):
+            self.handle_prev_command(connection)
+        elif command.startswith('!pause'):
+            self.handle_pause_command(connection)
+        elif command.startswith('!play'):
+            self.handle_play_command(connection)
+        elif command.startswith('!playlist'):
+            self.handle_playlist_command(connection, command)
+        elif command.startswith('!track'):
+            self.handle_track_command(connection, command)
+        elif command.startswith('!genre'):
+            self.handle_genre_command(connection, command)
+        elif command.startswith('!artist'):
+            self.handle_artist_command(connection, command)
+        elif command.startswith('!say'):
+            self.handle_say_command(connection, command)
+        elif command.startswith('!help'):
+            self.handle_help_command(connection)
+        else:
+            connection.privmsg(self.channel, f"Commande non reconnue : {command}")
 
-        elif message.startswith('!prev'):
-            _, error = execute_applescript('tell application "Music" to previous track')
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Piste précédente en cours de lecture\r\n".encode())
+    def execute_applescript(self, script):
+        process = subprocess.Popen(['osascript', '-e', script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+        if error:
+            return None, error.decode()
+        return output.decode().strip(), None
 
-        elif message.startswith('!pause'):
-            _, error = execute_applescript('tell application "Music" to pause')
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :La lecture de la musique a été mise en pause.\r\n".encode())
+    def find_partial_playlist(self, playlist_name, all_playlists):
+        matches = [p for p in all_playlists if re.search(playlist_name, p, re.IGNORECASE)]
+        return matches
 
-        elif message.startswith('!playlist'):
-            playlist_name_match = re.search(r'!playlist\s+(.+)', message)
-            if playlist_name_match:
-                playlist_name = playlist_name_match.group(1)
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: Nom de la playlist manquant.\r\n".encode())
-                continue
+    def handle_title_command(self, connection):
+        title, error = self.execute_applescript('tell application "Music" to get {name} of current track')
+        artist, error = self.execute_applescript('tell application "Music" to get {artist} of current track')
+        album, error = self.execute_applescript('tell application "Music" to get {album} of current track')
+        if title:
+            connection.privmsg(self.channel, f"TITRE: {title} ARTIST: {artist} ALBUM: {album}")
+        else:
+            connection.privmsg(self.channel, f"Erreur: {error}")
 
-            all_playlists_script = 'tell application "Music" to get name of playlists'
-            all_playlists, error = execute_applescript(all_playlists_script)
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                all_playlists = all_playlists.split(', ')
-                partial_matches = find_partial_playlist(playlist_name, all_playlists)
-                if partial_matches:
-                    selected_playlist = partial_matches[0]
-                    script = f'tell application "Music" to play playlist "{selected_playlist}"'
-                    _, error = execute_applescript(script)
-                    if error:
-                        irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-                    else:
-                        irc_socket.send(f"PRIVMSG {CHANNEL} :Lecture de la playlist : {selected_playlist}\r\n".encode())
-                else:
-                    irc_socket.send(f"PRIVMSG {CHANNEL} :Aucune playlist trouvée proche de : {playlist_name}\r\n".encode())
+    def handle_next_command(self, connection):
+        _, error = self.execute_applescript('tell application "Music" to next track')
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, "Piste suivante en cours de lecture")
 
-        elif message.startswith('!play'):
-            _, error = execute_applescript('tell application "Music" to play')
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :La lecture de la musique a été démarrée.\r\n".encode())
+    def handle_prev_command(self, connection):
+        _, error = self.execute_applescript('tell application "Music" to previous track')
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, "Piste précédente en cours de lecture")
 
-        elif message.startswith('!help'):
-            commands_list = "Liste des commandes disponibles : " \
-                    "!title - !next - !prev - !pause - !play - !playlist [nom_playlist] - !track [nom_piste] - !genre [nom_genre] - !artist [nom_artiste] - !say [texte à prononcer]"
-            irc_socket.send(f"PRIVMSG {CHANNEL} :{commands_list}\r\n".encode())
+    def handle_pause_command(self, connection):
+        _, error = self.execute_applescript('tell application "Music" to pause')
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, "La lecture de la musique a été mise en pause.")
 
-        elif message.startswith('!track'):
-            track_name_partial = message.split('!track', 1)[1].strip()
-            script = f'tell application "Music"\n'
-            script += f'set found_tracks to search playlist "général" for "{track_name_partial}"\n'
-            script += f'if found_tracks is not {{}} then\n'
-            script += f'    play item 1 of found_tracks\n'  # Jouer la première piste trouvée
-            script += f'else\n'
-            script += f'    set error_message to "Piste non trouvée : {track_name_partial}"\n'
-            script += f'end if\n'
-            script += f'end tell\n'
-            _, error = execute_applescript(script)
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Lecture de la piste contenant : {track_name_partial}\r\n".encode())
+    def handle_play_command(self, connection):
+        _, error = self.execute_applescript('tell application "Music" to play')
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, "La lecture de la musique a été démarrée.")
 
-        elif message.startswith('!genre'):
-            genre_name = message.split('!genre', 1)[1].strip()
-            script = f'set genre_name to "{genre_name}"\n'
-            script += f'tell application "Music"\n'
-            script += f'set genre_tracks to every track of playlist "général" whose genre is genre_name\n'
-            script += f'if genre_tracks is not {{}} then\n'
-            script += f'    try\n'
-            script += f'        delete every playlist whose name is "temp"\n'  # Supprimer la playlist "temp" si elle existe
-            script += f'    end try\n'
-            script += f'    set temp_playlist to make new playlist with properties {{name:"temp"}}\n'
-            script += f'    repeat with a_track in genre_tracks\n'
-            script += f'        duplicate a_track to temp_playlist\n'
-            script += f'    end repeat\n'
-            script += f'    play temp_playlist\n'  # Jouer la playlist "temp"
-            script += f'else\n'
-            script += f'end if\n'
-            script += f'end tell\n'
+    def handle_playlist_command(self, connection, command):
+        playlist_name_match = re.search(r'!playlist\s+(.+)', command)
+        if playlist_name_match:
+            playlist_name = playlist_name_match.group(1)
+        else:
+            connection.privmsg(self.channel, "Erreur: Nom de la playlist manquant.")
+            return
 
-            _, error = execute_applescript(script)
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Playlist temporaire créée avec les pistes du genre {genre_name}\r\n".encode())
-
-        elif message.startswith('!artist'):
-            artist_name = message.split('!artist', 1)[1].strip()
-            script = f'tell application "Music"\n'
-            script += f'set artist_tracks to search playlist "général" for "{artist_name}" only artists\n'
-            script += f'if artist_tracks is not {{}} then\n'
-            script += f'    try\n'
-            script += f'        delete every playlist whose name is "temp"\n'  # Supprimer la playlist "temp" si elle existe
-            script += f'    end try\n'
-            script += f'    set temp_playlist to make new playlist with properties {{name:"temp"}}\n'
-            script += f'    repeat with a_track in artist_tracks\n'
-            script += f'        duplicate a_track to temp_playlist\n'
-            script += f'    end repeat\n'
-            script += f'    play temp_playlist\n'  # Jouer la playlist "temp"
-            script += f'else\n'
-            script += f'end if\n'
-            script += f'end tell\n'
-
-            _, error = execute_applescript(script)
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur: {error}\r\n".encode())
-            else:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Playlist temporaire créée avec les pistes de {artist_name}\r\n".encode())
-
-        elif message.startswith('!say'):
-            text_to_say = message.split(' ', 1)[1]  # Extract the text to be spoken
-            print (text_to_say)
-            set_volume_script = 'tell application "Music" to set sound volume to 50'
-            _, error = execute_applescript(set_volume_script)
-            if error:
-                irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur lors de la modification du volume : {error}\r\n".encode())
-            else:
-                subprocess.run(['say', text_to_say])  # Utilise la commande 'say' pour prononcer le texte
-                set_volume_script = 'tell application "Music" to set sound volume to 100'
-                _, error = execute_applescript(set_volume_script)
+        all_playlists_script = 'tell application "Music" to get name of playlists'
+        all_playlists, error = self.execute_applescript(all_playlists_script)
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            all_playlists = all_playlists.split(', ')
+            partial_matches = self.find_partial_playlist(playlist_name, all_playlists)
+            if partial_matches:
+                selected_playlist = partial_matches[0]
+                script = f'tell application "Music" to play playlist "{selected_playlist}"'
+                _, error = self.execute_applescript(script)
                 if error:
-                    irc_socket.send(f"PRIVMSG {CHANNEL} :Erreur lors de la modification du volume : {error}\r\n".encode())
+                    connection.privmsg(self.channel, f"Erreur: {error}")
+                else:
+                    connection.privmsg(self.channel, f"Lecture de la playlist : {selected_playlist}")
+            else:
+                connection.privmsg(self.channel, f"Aucune playlist trouvée proche de : {playlist_name}")
+
+    def handle_track_command(self, connection, command):
+        track_name_partial = command.split('!track', 1)[1].strip()
+        script = f'tell application "Music"\n'
+        script += f'set found_tracks to search playlist "général" for "{track_name_partial}"\n'
+        script += f'if found_tracks is not {{}} then\n'
+        script += f'    play item 1 of found_tracks\n'
+        script += f'else\n'
+        script += f'    set error_message to "Piste non trouvée : {track_name_partial}"\n'
+        script += f'end if\n'
+        script += f'end tell\n'
+        _, error = self.execute_applescript(script)
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, f"Lecture de la piste contenant : {track_name_partial}")
+
+    def handle_genre_command(self, connection, command):
+        genre_name = command.split('!genre', 1)[1].strip()
+        script = f'set genre_name to "{genre_name}"\n'
+        script += f'tell application "Music"\n'
+        script += f'set genre_tracks to every track of playlist "général" whose genre is genre_name\n'
+        script += f'if genre_tracks is not {{}} then\n'
+        script += f'    try\n'
+        script += f'        delete every playlist whose name is "temp"\n'
+        script += f'    end try\n'
+        script += f'    set temp_playlist to make new playlist with properties {{name:"temp"}}\n'
+        script += f'    repeat with a_track in genre_tracks\n'
+        script += f'        duplicate a_track to temp_playlist\n'
+        script += f'    end repeat\n'
+        script += f'    play temp_playlist\n'
+        script += f'else\n'
+        script += f'end if\n'
+        script += f'end tell\n'
+        _, error = self.execute_applescript(script)
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, f"Playlist temporaire créée avec les pistes du genre {genre_name}")
+
+    def handle_artist_command(self, connection, command):
+        artist_name = command.split('!artist', 1)[1].strip()
+        script = f'tell application "Music"\n'
+        script += f'set artist_tracks to search playlist "général" for "{artist_name}" only artists\n'
+        script += f'if artist_tracks is not {{}} then\n'
+        script += f'    try\n'
+        script += f'        delete every playlist whose name is "temp"\n'
+        script += f'    end try\n'
+        script += f'    set temp_playlist to make new playlist with properties {{name:"temp"}}\n'
+        script += f'    repeat with a_track in artist_tracks\n'
+        script += f'        duplicate a_track to temp_playlist\n'
+        script += f'    end repeat\n'
+        script += f'    play temp_playlist\n'
+        script += f'else\n'
+        script += f'end if\n'
+        script += f'end tell\n'
+        _, error = self.execute_applescript(script)
+        if error:
+            connection.privmsg(self.channel, f"Erreur: {error}")
+        else:
+            connection.privmsg(self.channel, f"Playlist temporaire créée avec les pistes de {artist_name}")
+
+    def handle_say_command(self, connection, command):
+        text_to_say = command.split(' ', 1)[1]
+        set_volume_script = 'tell application "Music" to set sound volume to 50'
+        _, error = self.execute_applescript(set_volume_script)
+        if error:
+            connection.privmsg(self.channel, f"Erreur lors de la modification du volume : {error}")
+        else:
+            subprocess.run(['say', text_to_say])
+            set_volume_script = 'tell application "Music" to set sound volume to 100'
+            _, error = self.execute_applescript(set_volume_script)
+            if error:
+                connection.privmsg(self.channel, f"Erreur lors de la modification du volume : {error}")
+
+    def handle_help_command(self, connection):
+        help_message = "Commandes disponibles : !title, !next, !prev, !pause, !play, !playlist <nom>, !track <nom>, !genre <nom>, !artist <nom>, !say <message>"
+        connection.privmsg(self.channel, help_message)
+
+if __name__ == "__main__":
+    server = "your server adress "
+    port = 6667
+    channel = "#your channel""
+    nickname = "name of the bot"
+    bot = MusicBot(server, port, channel, nickname)
+    bot.start()
