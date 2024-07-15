923737fb17e2391e036380ff8a330ad2c4b7512b
Patricia charlotte BENEDETTO <110346262+cleobuline@users.noreply.github.com>
2024-07-15 21:30:11 +0200

    Update rosalie.py

--- rosalie.py
+++ rosalie.py
@@ -4,15 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import urllib.parse
+import json
 
 class YouTubeBot(irc.bot.SingleServerIRCBot):
     def __init__(self):
-        # YouTube API key and IRC server details
-        self.api_key = "YOUR_YOUTUBE_API_KEY"
-        self.server = "labynet.fr"  # set your preferred IRC server
+        self.api_key = "Google api here"
+        self.server = "labynet.fr"
         self.port = 6667
-        self.channel = "#labynet"  # set your preferred channel
-        self.nickname = "rosalie"  # set your preferred nick name
+        self.channel = "#labynet"
+        self.nickname = "rosalie"
         self.realname = "rosalie Bot"
         self.username = "rosalie"
         self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
@@ -20,33 +20,29 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
         irc.bot.SingleServerIRCBot.__init__(self, [(self.server, self.port)], self.nickname, self.realname)
 
     def on_welcome(self, connection, event):
-        # Join the specified channel upon successfully connecting to the IRC server
         connection.join(self.channel)
 
     def on_pubmsg(self, connection, event):
         try:
-            # Extract the message content
             message = event.arguments[0]
-
-            # Check for URLs in the message and get their titles
             urls = self.extract_urls(message)
             for url in urls:
                 if self.is_streaming_url(url):
-                    connection.privmsg(self.channel, f"Skipping streaming URL: {url}")
                     continue
-
-                title = self.get_page_title(url)
+                
+                if "youtube.com" in url or "youtu.be" in url:
+                    title = self.get_youtube_title(url)
+                else:
+                    title = self.get_page_title(url)
+                
                 if title:
                     connection.privmsg(self.channel, f"Title: {title}")
                 else:
                     connection.privmsg(self.channel, f"Could not retrieve title for: {url}")
 
-            # Respond to messages starting with !yt
             if message.startswith('!yt'):
                 query = message.split('!yt')[1].strip()
                 self.search_youtube(query, connection)
-
-            # Respond to messages starting with !gg
             elif message.startswith('!gg'):
                 query = message.split('!gg')[1].strip()
                 self.search_google(query, connection)
@@ -55,7 +51,6 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
             connection.privmsg(self.channel, "An unexpected error occurred. Please try again.")
 
     def search_youtube(self, query, connection):
-        # Options for youtube_dl to search YouTube
         ydl_opts = {
             'format': 'best',
             'quiet': True,
@@ -67,10 +62,8 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
         }
         with YoutubeDL(ydl_opts) as ydl:
             try:
-                # Extract information from YouTube based on the query
                 info_dict = ydl.extract_info("ytsearch:" + query, download=False)
                 if 'entries' in info_dict:
-                    # Retrieve the ID and title of the first matching video
                     video_info = info_dict['entries'][0]
                     video_id = video_info['id']
                     complete_url = f"https://www.youtube.com/watch?v={video_id}"
@@ -86,17 +79,14 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
 
     def search_google(self, query, connection):
         try:
-            # Search Google and retrieve the first URL
             headers = {'User-Agent': self.user_agent}
             url = f"https://www.google.com/search?q={query}"
             response = requests.get(url, headers=headers)
             response.raise_for_status()
             soup = BeautifulSoup(response.text, 'html.parser')
-            # Find the first non-Google link in the search results
             search_results = soup.find_all('div', class_='tF2Cxc')
             descriptions = soup.find_all('div', class_='VwiC3b')
             if search_results:
-                # Extract the URL from the first search result
                 url_tag = search_results[0].find('a', href=True)
                 if url_tag:
                     actual_url = url_tag['href']
@@ -113,17 +103,13 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
             connection.privmsg(self.channel, "An error occurred during the Google search.")
 
     def extract_urls(self, message):
-        # Extract URLs from the message
         return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
 
     def get_page_title(self, url):
-        # Retrieve the title of the webpage with timeout and error handling
         try:
             response = requests.get(url, headers={'User-Agent': self.user_agent}, timeout=10)
-            response.raise_for_status()  # Raise an exception for HTTP errors
+            response.raise_for_status()
             response.encoding = 'utf-8'
-            
-            # Check content type to handle non-HTML responses
             content_type = response.headers.get('Content-Type', '').lower()
             if 'text/html' in content_type:
                 soup = BeautifulSoup(response.text, 'html.parser')
@@ -132,24 +118,49 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
                 print(f"Skipping non-HTML content for URL: {url}")
                 return None
         except requests.exceptions.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")  # HTTP error
+            print(f"HTTP error occurred: {http_err}")
             return None
         except requests.exceptions.Timeout as timeout_err:
-            print(f"Timeout error occurred: {timeout_err}")  # Timeout error
+            print(f"Timeout error occurred: {timeout_err}")
             return None
         except requests.exceptions.RequestException as req_err:
-            print(f"Request error occurred: {req_err}")  # Other requests errors
+            print(f"Request error occurred: {req_err}")
             return None
         except Exception as e:
-            print(f"An error occurred in get_page_title: {e}")  # Other errors
+            print(f"An error occurred in get_page_title: {e}")
+            return None
+
+    def get_youtube_title(self, url):
+        try:
+            parsed_url = urllib.parse.urlparse(url)
+            if 'youtube.com' in parsed_url.netloc:
+                video_id = urllib.parse.parse_qs(parsed_url.query).get('v')
+            elif 'youtu.be' in parsed_url.netloc:
+                video_id = parsed_url.path.lstrip('/')
+            else:
+                return None
+            
+            if not video_id:
+                return None
+            
+            video_id = video_id[0] if isinstance(video_id, list) else video_id
+            api_url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={self.api_key}&part=snippet"
+            response = requests.get(api_url)
+            response.raise_for_status()
+            video_info = response.json()
+            items = video_info.get('items')
+            if items:
+                title = items[0]['snippet']['title']
+                return title
+            return None
+        except Exception as e:
+            print(f"Error in get_youtube_title: {e}")
             return None
 
     def is_streaming_url(self, url):
-        # Check if the URL contains "stream" in its path
         parsed_url = urllib.parse.urlparse(url)
         return 'stream' in parsed_url.path.lower()
 
 if __name__ == "__main__":
-    # Create an instance of the YouTubeBot and start it
     bot = YouTubeBot()
     bot.start()
